609d6d0b9a49724c07622aca446d44bc707e5838
Damagucci-Juice <jsr9874@gmail.com>
2022-04-13 16:26:04 +0900

    feat: 사용할 Notification Center와 관련한 설정 파일 생성

--- ios/TodoApp/TodoApp/Extensions/NotificationCenter.swift
+++ ios/TodoApp/TodoApp/Extensions/NotificationCenter.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension Notification.Name {
+    static let fetch = Notification.Name("fetch data from server")
+}
+
+enum NotificationKey {
+    case activity
+    case card
+    case column
+}
