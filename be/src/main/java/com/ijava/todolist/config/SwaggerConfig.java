2a54635cef2cc66c12f88e99ae046507b165f34d
jinan159 <wlsdhks0423@naver.com>
2022-04-14 17:52:52 +0900

    docs: Swagger 를 통한 API 문서 자동화 적용
    - Swagger, Springdoc 의존성 추가
    - 각 Controller, DTO 에 문서화 어노테이션 추가

---
+++ be/src/main/java/com/ijava/todolist/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.ijava.todolist.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("todo-list")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI todoListOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Todo-list API")
+                        .description("Todo-list API Documentation")
+                        .version("1.0")
+                );
+
+    }
+
+}
