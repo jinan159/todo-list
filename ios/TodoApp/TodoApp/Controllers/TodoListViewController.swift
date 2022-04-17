761a414f5982f7243ae97db092adfa0e1691fc5e
Song TaeHwan <axoghks@gmail.com>
2022-04-17 18:54:22 +0900

    feat: TodoListViewController 에 드래그 앤 드랍 기능 구현

--- ios/TodoApp/TodoApp/Controllers/TodoListViewController.swift
+++ ios/TodoApp/TodoApp/Controllers/TodoListViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 import OSLog
 
+protocol TodoListViewControllerDelegate {
+    func didStartDragging(from performer: TodoListViewController)
+    func didEndDropping()
+}
+
 class TodoListViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var badgeLabel: UILabel!
@@ -16,20 +21,23 @@ class TodoListViewController: UIViewController {
     
     // TODO: 커스텀 생성자(NSCoder) 만들어서 주입하기
     var viewModel: TodoListViewModel?
+    var delegate: TodoListViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
         self.addDelegates()
-        
-        self.viewModel?.onUpdate = { [weak self] in
-            DispatchQueue.main.async {
-                self?.badgeLabel?.text = String(self?.viewModel?.count ?? 0)
-                self?.tableView.reloadData()
-            }
-        }
+        self.bind()
         
         self.viewModel?.fetchData()
+    }
+    
+    private func bind() {
+        self.viewModel?.models.bind { [weak self] models in
+            self?.badgeLabel?.text = String(self?.viewModel?.count ?? .zero)
+            // 최적화 필요
+            self?.tableView.reloadData()
+        }
     }
     
     private func addDelegates() {
@@ -40,10 +48,15 @@ class TodoListViewController: UIViewController {
     
     private func configureUI() {
         self.titleLabel?.text = self.viewModel?.getHeaderTitle()
-        self.badgeLabel?.text = String(self.viewModel?.count ?? 0)
+        self.badgeLabel?.text = String(self.viewModel?.count ?? .zero)
         
         self.badgeView.clipsToBounds = true
         self.badgeView.layer.cornerRadius = self.badgeView.frame.size.height / 2
+    }
+    
+    func reset() {
+        guard let section = self.viewModel?.selection else { return }
+        self.tableView.deleteSections(IndexSet(integer: section), with: .automatic)
     }
 }
 
@@ -65,7 +78,7 @@ extension TodoListViewController: UITableViewDataSource {
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return self.viewModel?.count ?? 0
+        return self.viewModel?.count ?? .zero
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,7 +86,7 @@ extension TodoListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 0
+        return .zero
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -105,11 +118,11 @@ extension TodoListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
-
+    
     func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
         let config = UIContextMenuConfiguration(
             identifier: nil,
-            previewProvider: nil) { _ in
+            previewProvider: nil) { [weak self] _ in
                 let move = UIAction(
                     title: "완료한 일로 이동",
                     image: UIImage(systemName: "folder"),
@@ -129,6 +142,7 @@ extension TodoListViewController: UITableViewDelegate {
                     image: UIImage(systemName: "trash"),
                     attributes: .destructive,
                     state: .off) { _ in
+                        guard let self = self else { return }
                         self.viewModel?.remove(at: indexPath.section)
                         tableView.deleteSections(IndexSet(integer: indexPath.section), with: .fade)
                     }
@@ -141,5 +155,69 @@ extension TodoListViewController: UITableViewDelegate {
             }
         
         return config
+    }
+}
+
+extension TodoListViewController: UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        guard let model = self.viewModel?[indexPath.section] else {
+            fatalError()
+        }
+        
+        self.viewModel?.selection = indexPath.section
+        
+        let itemProvider = NSItemProvider()
+        let item = UIDragItem(itemProvider: itemProvider)
+        item.localObject = model
+        
+        return [item]
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, canHandle session: UIDropSession) -> Bool {
+        return session.items.count == 1
+    }
+    
+    func tableView(_ tableView: UITableView, dragSessionWillBegin session: UIDragSession) {
+        // delegate 호출
+        self.delegate?.didStartDragging(from: self)
+    }
+}
+
+extension TodoListViewController: UITableViewDropDelegate {
+    func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+        var dropProposal = UITableViewDropProposal(operation: .cancel)
+        
+        guard session.items.count == 1 else { return dropProposal }
+        
+        if tableView.hasActiveDrag {
+            dropProposal = UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        } else {
+            dropProposal = UITableViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+        }
+        
+        return dropProposal
+    }
+    
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        let destinationIndexPath: IndexPath
+        
+        if let indexPath = coordinator.destinationIndexPath {
+            destinationIndexPath = indexPath
+        } else {
+            let section = tableView.numberOfSections == .zero ? .zero : tableView.numberOfSections - 1
+            let row = max(tableView.numberOfRows(inSection: section), 1)
+            destinationIndexPath = IndexPath(row: row, section: section)
+        }
+        
+        guard let item = coordinator.session.items.first else { return }
+        guard let model = item.localObject as? Todo else { return }
+        
+        self.viewModel?.insert(data: model, at: destinationIndexPath.section)
+        self.tableView.insertRows(at: [destinationIndexPath], with: .automatic)
+        self.delegate?.didEndDropping()
     }
 }
--- ios/TodoApp/TodoApp/Models/TodoListViewModel.swift
+++ ios/TodoApp/TodoApp/Models/TodoListViewModel.swift
@@ -11,29 +11,28 @@ protocol TodoListViewModelProtocol {
     func fetchData()
     func remove(at index: Int)
     func getHeaderTitle() -> String
-    var onUpdate: () -> Void { get }
     var count: Int { get }
 }
 
 final class TodoListViewModel: TodoListViewModelProtocol {
     private let repository: TodoRepositoryProtocol
     private let column: Column
-    private var models = [Todo]()
+    private(set) var models = Observable(value: [Todo]())
+    
+    var selection: Int?
     
     subscript(index: Int) -> Todo? {
         get {
-            guard index < self.models.count && index >= self.models.startIndex else {
+            guard index < self.models.value.count && index >= self.models.value.startIndex else {
                 return nil
             }
             
-            return self.models[index]
+            return self.models.value[index]
         }
     }
     
-    var onUpdate: () -> Void = {}
-    
     var count: Int {
-        return self.models.count
+        return self.models.value.count
     }
     
     init(entity column: Column, repository: TodoRepositoryProtocol) {
@@ -43,13 +42,23 @@ final class TodoListViewModel: TodoListViewModelProtocol {
     
     func remove(at index: Int) {
         // TODO: 서버에 삭제 요청
-        self.models.remove(at: index)
+        self.models.value.remove(at: index)
+    }
+    
+    func insert(data: Todo, at index: Int) {
+        if self.models.value.isEmpty {
+            self.models.value.append(data)
+            return
+        }
+        
+        self.models.value.insert(data, at: index)
     }
     
     func fetchData() {
-        self.repository.fetchTodo(from: self.column) { models in
-            self.models = models
-            self.onUpdate()
+        self.repository.fetchTodo(from: self.column) { [weak self] models in
+            DispatchQueue.main.async {
+                self?.models.value = models
+            }
         }
     }
     
